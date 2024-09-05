8f720c81e8128335a2453f367b98deab3a94b29c
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-05 09:49:45 -0600

    Updated to now to Fahrenhiet and Kevlin.

--- Celsius to Fahrenhiet/main.py
+++ Celsius to Fahrenhiet/main.py
@@ -1,7 +1,29 @@
 #Sawyer Wood, Celsius to Fahrenhiet calculator.
 
-Celsius = input("What Celsius temperature is it?: ")
-Celsius = int(Celsius)
-Fahrenhiet = (Celsius * (9/5))+32
+Inputype = input("What type of degrees are you putting into the claclulator? F or C or K: ")
 
-print (Celsius, "degrees Celsius is" ,Fahrenhiet, "degrees Fahrenhiet")
+if Inputype == str("C"):
+
+    Celsius = input("What Celsius temperature is it?: ")
+    Celsius = int(Celsius)
+    Fahrenhiet = (Celsius * (9/5))+32
+    Kelvin = (Celsius + 273.15)
+
+    print (Celsius, "degrees Celsius is" ,Fahrenhiet, "degrees Fahrenhiet and" ,Kelvin, "degrees Kelvin.")
+
+if Inputype == str("F"):
+    Fahrenhiet = input("What Fahrenhiet temperature is it?: ")
+    Fahrenhiet = int(Fahrenhiet)
+    Celsius = (Fahrenhiet - 32)*(5/9)
+    Kelvin = (Celsius +273.15)
+
+    print (Fahrenhiet, "degrees Fahrenhiet is" ,Celsius, "Degrees Celsius and" ,Kelvin, "degrees Kelvin.")
+
+if Inputype == str("K"):
+
+    Kelvin = input("What Kelvin is it?: ")
+    Kelvin = int(Kelvin)
+    Celsius = (Kelvin - 273.15)
+    Fahrenhiet = (Celsius * 9/5)+32
+
+    print (Kelvin, "degrees Kelvin is" ,Celsius, "degrees Celsius and" ,Fahrenhiet, "degrees Fahrenhiet.")
