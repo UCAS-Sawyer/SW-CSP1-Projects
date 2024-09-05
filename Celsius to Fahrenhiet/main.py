bf1513004c0ee5e431ca4f2d7ee169a99294f898
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-05 09:26:52 -0600

    It is a working Celsius to Fahrenhiet calculator.

---
+++ Celsius to Fahrenhiet/main.py
@@ -0,0 +1,7 @@
+#Sawyer Wood, Celsius to Fahrenhiet calculator.
+
+Celsius = input("What Celsius temperature is it?: ")
+Celsius = int(Celsius)
+Fahrenhiet = (Celsius * (9/5))+32
+
+print (Celsius, "degrees Celsius is" ,Fahrenhiet, "degrees Fahrenhiet")
