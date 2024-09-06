5514711d45a82d722d2c0f4bad67a12924ed66fc
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-06 09:51:19 -0600

    Still working on it.

---
+++ Pythagorean Calculator/main.py
@@ -0,0 +1,30 @@
+#This is for fun
+
+#So I can do math :|
+import math
+
+Outputwanted = input("What side of the triandle are you trying to find, Leg or Hypotenuse?: ")
+
+if Outputwanted == str("l"):
+  Leg1Length = input("What is the length of leg 1?: ")
+  Hypotenuse = input("What is the legnth of the hypotenuse?: ")
+  Leg1Length = int(Leg1Length)
+  Hypotenuse = int(Hypotenuse)
+  Leg2Length = (math.sqrt((Hypotenuse **2) - (Leg1Length **2)))
+
+  if Leg2Length < 0:
+      Leg2Length = str(Leg2Length)
+      print ("This is not a possible triangle.")
+    
+  if Leg2Length > 0:
+     Leg2Length = str(Leg2Length)
+     print ("The length of the leg is", Leg2Length)
+
+if Outputwanted == str("h"):
+  Leg1Length = input("What is the length of leg 1?: ")
+  Leg2Length = input("What is the length of leg 2?: ")
+  Leg1Length = int(Leg1Length)
+  Leg2Length = int(Leg2Length)
+  Hypotenuse = (math.sqrt((Leg1Length **2) + (Leg2Length **2)))
+  Hypotenuse = str(Hypotenuse)
+  print ("The length of the hypotenuse is", Hypotenuse)
--- awards_tables/main.py
+++ awards_tables/main.py
@@ -4,10 +4,13 @@ students = 100
 students = students-1
 seats = 12
 staff = 32
+
 staff = staff-3
-numberofguestsperstudent = ((students-15)*2)+15
+numberofguestsperstudent = (((students-15)*2)+15)
 schoolboardmember = 1
 
 
 
 print((staff+students+numberofguestsperstudent+schoolboardmember)/seats)
+
+print((30+99+183)/12)
