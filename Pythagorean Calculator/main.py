11c16cfbe97690c7de328633ff4cf546a87ee44e
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-06 19:19:41 -0600

    Update

--- Pythagorean Calculator/main.py
+++ Pythagorean Calculator/main.py
@@ -1,27 +1,24 @@
 #This is for fun
-#Find out what is wrong with the Outputwanted not like having to options it could be for the "if" statements.
 
 import math
 
 Outputwanted = input("What side of the triandle are you trying to find, Leg or Hypotenuse?: ")
 
-if Outputwanted == str("l") or str("Leg"):
+if Outputwanted == str("L" or "Leg"):
   Leg1Length = input("What is the length of leg 1?: ")
   Hypotenuse = input("What is the legnth of the hypotenuse?: ")
   Leg1Length = int(Leg1Length)
   Hypotenuse = int(Hypotenuse)
-  Leg2Length = (math.sqrt((Hypotenuse **2) - (Leg1Length **2)))
-  Leg2Length = int(Leg2Length)
 
-  if Leg2Length < 0:
-      Leg2Length = str(Leg2Length)
-      print ("This is not a possible triangle.")
-    
-  if Leg2Length > 0:
-     Leg2Length = str(Leg2Length)
-     print ("The length of the leg is", Leg2Length)
+  if Hypotenuse <= Leg1Length:
+    print ("This is not a triangle")
+  else:
+    Leg2Length = (math.sqrt((Hypotenuse **2) - (Leg1Length **2)))
+    Leg2Length = str(Leg2Length)
+    print ("The length of the leg is", Leg2Length)
 
-if Outputwanted == str("h") or str("Hypotenuse"):
+
+if Outputwanted == str("H" or "Hypotenuse"):
   Leg1Length = input("What is the length of leg 1?: ")
   Leg2Length = input("What is the length of leg 2?: ")
   Leg1Length = int(Leg1Length)
