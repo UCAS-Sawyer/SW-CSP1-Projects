d15020b633d6bcd9693379e76a2cb1709bcdb1c3
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-10 09:49:51 -0600

    It makes an adlib yay

---
+++ Madlib/main.py
@@ -0,0 +1,7 @@
+#Sawyer Wood, madlib maker 6000.
+word1 = input("What is your first (Noun) going to be?: ")
+word2 = input("What is your second (Noun) going to be?: ")
+word3 = input("What is your (Verb) going to be?: ")
+print("It was a beautifull day when you saw a", word1, """you were confuse and you kept walking. Adventualy you came apon a""", 
+      word2, """you thought it wierd, but you kept walking along the path. What felt like a couple seconds later you saw a""", 
+      word1, "and a", word2, word3)
