4c6c48e7a3cb94c5a5eb9febb39a920d192ee53d
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-10 09:35:50 -0600

    finished

---
+++ Palindrome Finder/main.py
@@ -0,0 +1,7 @@
+#Sawyer Wood, SkillPractice: Palindrome
+
+word = input("What is the word you want to put int?: " )
+if word == word [::-1]:
+    print("This is a palindrome")
+else:
+    print("This is not a palindrome")
