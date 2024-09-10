8205ecb3a4b11480f7566d493b0159c0da6cf30e
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-10 09:39:05 -0600

    Can now do words with capitals

--- Palindrome Finder/main.py
+++ Palindrome Finder/main.py
@@ -1,6 +1,7 @@
 #Sawyer Wood, SkillPractice: Palindrome.
 
-word = input("What is the word you want to put int?: " )
+word = input("What is the word you want to put in it?: " )
+word = str.lower(word)
 if word == word [::-1]:
     print("This is a palindrome")
 else:
