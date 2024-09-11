6051a1e56fadf8def78211f46e90a36066f9c251
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-11 09:52:31 -0600

    Workinprogress

---
+++ Average Grade Calculator/main.py
@@ -0,0 +1,9 @@
+# Sawyer Wood, for the average grade skillpractice.
+
+class1 = input("What is the grade for your first period(Not a letter grade please)?: ")
+class2 = input("What is the grade for your second period?: ")
+class3 = input("What is the grade for your third period?: ")
+Advisory = input("What is the grade for Advisory?: ")
+class6 = input("What is the grade for your sixth period?: ")
+class7 = input("What is the grade for your seventh period?: ")
+class8 = input("What is the grade for your eighth period?: ")
