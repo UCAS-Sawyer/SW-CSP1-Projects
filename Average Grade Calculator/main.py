87a1d6fcfb0697a295866f8bc2c21b7816ec5eea
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-12 09:20:55 -0600

    Updates

--- Average Grade Calculator/main.py
+++ Average Grade Calculator/main.py
@@ -1,9 +1,14 @@
 # Sawyer Wood, for the average grade skillpractice.
 
-class1 = input("What is the grade for your first period(Not a letter grade please)?: ")
-class2 = input("What is the grade for your second period?: ")
-class3 = input("What is the grade for your third period?: ")
-Advisory = input("What is the grade for Advisory?: ")
-class6 = input("What is the grade for your sixth period?: ")
-class7 = input("What is the grade for your seventh period?: ")
-class8 = input("What is the grade for your eighth period?: ")
+class1 = float(input("What is the grade for your first period(Not a letter grade please)?: "))
+class2 = float(input("What is the grade for your second period?: "))
+class3 = float(input("What is the grade for your third period?: "))
+Advisory = float(input("What is the grade for Advisory?: "))
+class6 = float(input("What is the grade for your sixth period?: "))
+class7 = float(input("What is the grade for your seventh period?: "))
+class8 = float(input("What is the grade for your eighth period?: "))
+
+GradesCombined = class1 + class2 + class3 + Advisory + class6 + class7 + class8
+AverageGrade = GradesCombined/7
+AverageGrade = round(AverageGrade, 2)
+print("Your average grade is", AverageGrade, "%")
