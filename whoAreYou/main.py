233728c572b39aad497a2d5cbe630313c150b560
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-13 09:53:19 -0600

    it works

---
+++ whoAreYou/main.py
@@ -0,0 +1,7 @@
+#Sawyer Wood, Who are you skill practice
+
+name = input("What is your name?: ")
+age = input("how old are you?: ")
+favcolor = input("What is your favorite color?: ")
+
+Sentance = ("Your name is", name, "your age is", age, "and you like the color", favcolor, ".")
