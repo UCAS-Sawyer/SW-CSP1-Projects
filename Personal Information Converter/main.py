c42417263a6d4ef4920116d1bc58f5ed46748a5a
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-18 09:24:05 -0600

    It exists

---
+++ Personal Information Converter/main.py
@@ -0,0 +1,12 @@
+#Sawyer Wood, Personal Information Converter proficiency test.
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+favnum = input("What is your favorite number?: ")
+height = input("How tall are you(In meters)?: ")
+ageV2 = int(age)
+favenumV2 = int(favnum)
+heightV2= float(height)
+
+print("Your original inputs were name:", name, ", age:", age, ", favorite number:",favnum, ", and hieght:", height, "m")
+print("Your information after we put it into the infoconverter9000 is, name:", name,", age:", ageV2,", favorite number:", favenumV2,", and hieght:",height, "m")
