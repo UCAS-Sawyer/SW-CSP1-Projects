ac8ff3a8f8096186e023ac12d623535475916779
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-21 16:34:24 -0600

    updates

--- FunProject01/main.py
+++ FunProject01/main.py
@@ -1,4 +1,5 @@
 #4fun
+import keyboard
 import random
 money = 100
 prize1 = 5
@@ -8,40 +9,49 @@ prize4 = 70
 prize5 = 145
 jackpot = 300
 casinogain = 0
+spincount = 0
+print("Starting")
 while money > 0:
-    spin = input("Spin?: ")
-    if spin == str("Yes"):
-        money = money - 15
-        casinogain = casinogain + 15
+
+    if keyboard.read_key() == "p":
+        money = 0
+        print("You forfeit")
+
+    if keyboard.read_key() == "c":
+        print(spincount)
+
+    if keyboard.read_key() == "s":
+        spincount = spincount + 1
+        money = money - 20
+        casinogain = casinogain + 20
         num = random.randint(0,100)
-        print("You rolled", num)
         if num <= 50:
             money = money + prize1
             casinogain = casinogain - prize1
-            print("You won", prize1, "dollors!")
+            print("You rolled", num, "You won", prize1, "dollars!")
             
         if 75 >= num > 50:
             money = money + prize2
             casinogain = casinogain - prize2
-            print("You won", prize2, "dollors!")
+            print("You rolled", num, "You won", prize2, "dollars!")
 
         if 87 >= num > 75:
             money = money + prize3
             casinogain = casinogain - prize3
-            print("You won", prize3, "dollors!")
+            print("You rolled", num, "You won", prize3, "dollars!")
 
         if 93 >= num > 87:
             money = money + prize4
             casinogain = casinogain - prize4
-            print("You won", prize4, "dollors!")
+            print("You rolled", num, "You won", prize4, "dollars!")
         
         if 99 >= num > 93:
             money = money + prize5
             casinogain = casinogain - prize5
-            print("You won", prize5, "dollors!")
+            print("You rolled", num, "You won", prize5, "dollars!")
         
         if num == 100:
             money = money + jackpot
             casinogain = casinogain - jackpot
-            print("You won", jackpot, "dollors!")
-    print("You currently have", money, "dollors and the casino has made",casinogain, "dollors!")
+            print("You won", jackpot, "dollars!")
+    print("You currently have", money, "dollars and the casino has made",casinogain, "dollars!")
