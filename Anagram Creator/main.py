18831eeed5846109f7ad054501ef35790a9fbc4d
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-23 09:49:11 -0600

    updates

---
+++ Anagram Creator/main.py
@@ -0,0 +1,7 @@
+#Sawyer Wood, Anagram Creator RAID
+import random
+
+name = input("What is your name?: ")
+num = len(name)
+name = [*name]
+print(name[0:num])
--- Calculator9000/main.py
+++
@@ -1,3 +0,0 @@
-#4fun
-print("The types of calculators available right now are...")
-input("What calculator do you want?: ")
