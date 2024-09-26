ba588ef2ef703c70ffacd8188d6eb427383c2ad9
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-09-26 16:54:55 -0600

    updates

--- Cipherifyer9000/main.py
+++ Cipherifyer9000/main.py
@@ -1,14 +1,14 @@
 #Sawyer Wood, ProficiencyTest Secret Sypher.
-word = input("What is the word you want to Cipherify?: ")
+word = input("What is the word you want to Cipherify/Decipherify?: ")
 idk = input("Do you want to cipherify or decipherify?: ")
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-shiftedalphabet = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b"]
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "1"]
+shiftedalphabet = ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", ":", "1"]
 
 def Cipherify(word):
     wordspot = 0
     alphabetspot = 0
     stopoint = len(word)
-
+    print(stopoint)
     while wordspot < stopoint:
         if word[wordspot] == alphabet[alphabetspot]:
             letter = shiftedalphabet[alphabetspot]
@@ -28,11 +28,11 @@ def Decipherify(word):
             letter = alphabet[alphabetspot]
             wordspot = wordspot + 1
             alphabetspot = 0
-            print(letter)
+            print( letter)
         else:
             alphabetspot = alphabetspot + 1
 
-if idk == "c":
+if idk == ["c", "cipher", "C", "Cipher"]:
     Cipherify(word)
 else:
     Decipherify(word)
