b4c9e72ee52cec46194b8a319d708b600e15fa62
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-10-01 09:52:29 -0600

    testststststs

--- TestingFile/test.py
+++ TestingFile/test.py
@@ -1,6 +1,16 @@
-num2 = 4
 
-def plusone(*num):
-    return(1 + num[num2])
+def sum_even_numbers(numbers):
 
-print(plusone(6, 3, 5, 10, 327, 1, 35, 84))
+     sum = 0
+
+     for num in numbers:
+
+          if num % 2 == 0:
+
+               sum += num
+
+          else:
+
+               return 0
+
+     return sum
