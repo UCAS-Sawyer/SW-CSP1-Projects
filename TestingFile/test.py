232fd2fd0f2030cf9f72c4fb0f7a5e89b8d53db2
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-10-02 09:36:05 -0600

    testststst

--- TestingFile/test.py
+++ TestingFile/test.py
@@ -1,16 +1,16 @@
 def sum_even_numbers(numbers):
 
      sum = 0
-     
+     numbers = str(numbers)
      for num in numbers:
-          
+          num = int(num)
           if num % 2 == 0:
 
                sum += num
 
-          elif num % 2 ==0:
+          elif num % 2 !=0:
                #This is just doing nothing and moving on to the next number
-               print()
+               x = "fun"
 
      return sum
-print(sum_even_numbers(1232133))
+print(sum_even_numbers(1232696532414336547658989916325421133))
