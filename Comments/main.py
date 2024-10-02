b1ff68e95a6793da7faaa58ace8b2486a1f5b2d7
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-10-02 09:42:00 -0600

    updates

---
+++ Comments/main.py
@@ -0,0 +1,15 @@
+#Sawyer Wood, Video 1 comments challenge.
+#-----------------------------------------------------------------
+# Name:         Hello program
+#
+# Author:       Sawyer
+#
+# Created:      10/2/2024
+# Licence:      (None)
+#-----------------------------------------------------------------
+
+#Asking for the users name
+name = input("What is your name?") 
+
+#Saying hello to the user with their inputed name.
+print("Hello", name) 
