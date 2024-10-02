99988a959d262c893a266269a7a40d4b79ed05ab
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-10-02 09:21:01 -0600

    tetstststs

--- TestingFile/test.py
+++ TestingFile/test.py
@@ -1,16 +1,16 @@
-
 def sum_even_numbers(numbers):
 
      sum = 0
-
+     
      for num in numbers:
-
+          
           if num % 2 == 0:
 
                sum += num
 
-          else:
-
-               return 0
+          elif num % 2 ==0:
+               #This is just doing nothing and moving on to the next number
+               print()
 
      return sum
+print(sum_even_numbers(1232133))
