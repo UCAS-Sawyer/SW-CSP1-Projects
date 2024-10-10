7f24ccc39b3c8bd6a2736c60c57e90a5e03a9858
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2024-10-10 09:50:31 -0600

    updates

--- NumberQuessingGame7000/main.py
+++ NumberQuessingGame7000/main.py
@@ -1,22 +1,22 @@
 #Sawyer Wood, Number Guessing Game Raid.
 
-import random
+import random #Import random so you can generate random numbers
 
-Goal = random.randint(1, 10)
+Goal = random.randint(1, 10) #Generating the random number
 
-Guess = int(input("What to do you think the number is, it is between 1 and 10?: "))
+Guess = int(input("What to do you think the number is, it is between 1 and 10?: ")) #Getting their guess
 
 
-while True:
+while True: #Forever loop
 
-    if Guess > Goal:
-        print("Your guess was too high, try again.")
-        Guess = int(input("What to do you think the number is, it is between 1 and 10?: "))
+    if Guess > Goal: #If their guess was too high
+        print("Your guess was too high, try again.") #Print it was too high
+        Guess = int(input("What to do you think the number is, it is between 1 and 10?: "))#Getting their guess
 
-    if Guess == Goal:
-        print("You guessed the right number!")
-        break
+    if Guess == Goal: #If they guess right
+        print("You guessed the right number!") #Print they guessed right
+        break #Stop the program
 
-    if Guess < Goal:
-        print("Your guess was too low, try again.")
-        Guess = int(input("What to do you think the number is, it is between 1 and 10?: "))
+    if Guess < Goal: #If their guess was too low
+        print("Your guess was too low, try again.") #Print it was too low
+        Guess = int(input("What to do you think the number is, it is between 1 and 10?: "))#Getting their guess
